2ae3a34cb03cdde9799cee2d3353befa0a2a51a4
sven <sven@so36.net>
2020-01-03 12:28:06 +0100

    refactor(script): entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,89 +1,36 @@
-#!/bin/sh -l
+#!/bin/sh
 
 set -e
-
-if [ -z "$ACCESS_TOKEN" ] && [ -z "$GITHUB_TOKEN" ]
-then
-  echo "You must provide the action with either a Personal Access Token or the GitHub Token secret in order to deploy."
-  exit 1
-fi
-
-if [ -z "$BRANCH" ]
-then
-  echo "You must provide the action with a branch name it should deploy to, for example gh-pages or docs."
-  exit 1
-fi
-
-if [ -z "$FOLDER" ]
-then
-  echo "You must provide the action with the folder name in the repository where your compiled page lives."
-  exit 1
-fi
-
-case "$FOLDER" in /*|./*)
-  echo "The deployment folder cannot be prefixed with '/' or './'. Instead reference the folder name directly."
-  exit 1
-esac
-
-
-# Gets the commit email/name if it exists in the push event payload.
-COMMIT_EMAIL=$(jq '.pusher.email' "${GITHUB_EVENT_PATH}")
-COMMIT_NAME=$(jq '.pusher.name' "${GITHUB_EVENT_PATH}")
-
-# If the commit email/name is not found in the event payload then it falls back to the actor.
-if [ -z "$COMMIT_EMAIL" ]
-then
-  COMMIT_EMAIL="${GITHUB_ACTOR:-github-pages-deploy-action}@users.noreply.github.com"
-fi
-
-if [ -z "$COMMIT_NAME" ]
-then
-  COMMIT_NAME="${GITHUB_ACTOR:-GitHub Pages Deploy Action}"
-fi
-
-# Directs the action to the the Github workspace.
-cd "$GITHUB_WORKSPACE" && \
-
-# Configures Git.
-git init && \
-git config --global user.email "${COMMIT_EMAIL}" && \
-git config --global user.name "${COMMIT_NAME}" && \
-
-## Initializes the repository path using the access token.
-REPOSITORY_PATH="https://${ACCESS_TOKEN:-"x-access-token:$GITHUB_TOKEN"}@github.com/${GITHUB_REPOSITORY}.git" && \
-
-# Checks to see if the remote exists prior to deploying.
-# If the branch doesn't exist it gets created here as an orphan.
-if [ "$(git ls-remote --heads "$REPOSITORY_PATH" "$BRANCH" | wc -l)" -eq 0 ];
-then
-  echo "Creating remote branch ${BRANCH} as it doesn't exist..."
-  git checkout ${BASE_BRANCH:-master} && \
-  git checkout --orphan "$BRANCH" && \
-  git rm -rf . && \
-  touch README.md && \
-  git add README.md && \
-  git commit -m "Initial ${BRANCH} commit" && \
-  git push "$REPOSITORY_PATH" "$BRANCH"
-fi
-
-# Checks out the base branch to begin the deploy process.
-git checkout ${BASE_BRANCH:-master} && \
 
 # Builds the project if a build script is provided.
 echo "Running build scripts... $BUILD_SCRIPT" && \
 eval "$BUILD_SCRIPT" && \
 
-if [ "$CNAME" ]; then
-  echo "Generating a CNAME file in in the $FOLDER directory..."
-  echo "$CNAME" > "$FOLDER"/CNAME
-fi
+echo "#################################################"
+echo "Changing directory to 'BUILD_DIR' $BUILD_DIR ..."
+cd $BUILD_DIR
 
-# Commits the data to Github.
-echo "Deploying to GitHub..." && \
-cd "./${FOLDER}"
-git add -A . && \
-
-git commit -m "Deploying to ${BRANCH} from ${BASE_BRANCH:-master} ${GITHUB_SHA}" --quiet && \
-git push "$REPOSITORY_PATH" $(git subtree split --prefix "$FOLDER" ${BASE_BRANCH:-master}):"$BRANCH" --force && \
-
-echo "Deployment succesful!"
+echo "#################################################"
+echo "Now deploying to GitHub Pages..."
+REMOTE_REPO="https://${ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
+REPONAME="$(echo $GITHUB_REPOSITORY| cut -d'/' -f 2)" && \
+OWNER="$(echo $GITHUB_REPOSITORY| cut -d'/' -f 1)" && \
+GHIO="${OWNER}.github.io" && \
+if [[ "$REPONAME" == "$GHIO" ]]; then
+  REMOTE_BRANCH="master"
+else
+  REMOTE_BRANCH="gh-pages"
+fi && \
+git init && \
+git config user.name "${GITHUB_ACTOR}" && \
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
+if [ -z "$(git status --porcelain)" ]; then
+    echo "Nothing to commit" && \
+    exit 0
+fi && \
+git add . && \
+git commit -m 'Deploy to GitHub Pages' && \
+git push --force $REMOTE_REPO master:$REMOTE_BRANCH && \
+rm -fr .git && \
+cd $GITHUB_WORKSPACE && \
+echo "Content of $BUILD_DIR has been deployed to GitHub Pages."
